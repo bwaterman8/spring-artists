5abb23dbbefce514915ac4ce30529d2cc18c17f9
bwaterman8 <bwaterman8@gmail.com>
2021-09-02 12:13:39 -0400

    extracted from zip

---
+++ src/main/java/com/artistcomplete/springartists/SpringArtistsApplication.java
@@ -0,0 +1,13 @@
+package com.artistcomplete.springartists;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringArtistsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringArtistsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/artistcomplete/springartists/SpringArtistsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artistcomplete.springartists;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringArtistsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
