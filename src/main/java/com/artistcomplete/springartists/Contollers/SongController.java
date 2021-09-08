4de4057d13e899860b5aeb2279980f00e3d112e0
bwaterman8 <bwaterman8@gmail.com>
2021-09-08 12:03:37 -0400

    connected song template to controller

--- src/main/java/com/artistcomplete/springartists/Contollers/SongController.java
+++ src/main/java/com/artistcomplete/springartists/Contollers/SongController.java
@@ -1,5 +1,6 @@
 package com.artistcomplete.springartists.Contollers;
 
+import com.artistcomplete.springartists.Repos.AlbumRepository;
 import com.artistcomplete.springartists.Repos.SongRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,9 +10,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class SongController {
 
     private final SongRepository songRepository;
+    private final AlbumRepository albumRepository;
 
-    public SongController(SongRepository songRepository) {
+    public SongController(SongRepository songRepository, AlbumRepository albumRepository) {
         this.songRepository = songRepository;
+        this.albumRepository = albumRepository;
     }
     @RequestMapping("/songs")
     public String getSongs(Model model) {
