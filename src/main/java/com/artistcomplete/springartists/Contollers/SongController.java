914714f34c154a394518178b08c6e15f738f4907
bwaterman8 <bwaterman8@gmail.com>
2021-09-08 11:54:28 -0400

    filled out song controller and template

--- src/main/java/com/artistcomplete/springartists/Contollers/SongController.java
+++ src/main/java/com/artistcomplete/springartists/Contollers/SongController.java
@@ -1,4 +1,24 @@
 package com.artistcomplete.springartists.Contollers;
 
+import com.artistcomplete.springartists.Repos.SongRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class SongController {
+
+    private final SongRepository songRepository;
+
+    public SongController(SongRepository songRepository) {
+        this.songRepository = songRepository;
+    }
+    @RequestMapping("/songs")
+    public String getSongs(Model model) {
+
+        model.addAttribute("songs", songRepository.findAll());
+
+        return "songs";
+    }
+
 }
