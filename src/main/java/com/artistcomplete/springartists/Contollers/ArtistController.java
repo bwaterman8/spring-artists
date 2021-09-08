8b1fd2133fb644c105642fb0d07cce55952e119e
bwaterman8 <bwaterman8@gmail.com>
2021-09-08 11:11:46 -0400

    made artist controller and created artist view

--- src/main/java/com/artistcomplete/springartists/Contollers/ArtistController.java
+++ src/main/java/com/artistcomplete/springartists/Contollers/ArtistController.java
@@ -1,8 +1,23 @@
 package com.artistcomplete.springartists.Contollers;
 
+import com.artistcomplete.springartists.Repos.ArtistRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class ArtistController {
 
-    public String getArtists() {
+    private final ArtistRepository artistRepository;
+
+    public ArtistController(ArtistRepository artistRepository) {
+        this.artistRepository = artistRepository;
+    }
+
+    @RequestMapping("/artists")
+    public String getArtists(Model model) {
+
+        model.addAttribute("artists", artistRepository.findAll());
 
         return "artists";
     }
