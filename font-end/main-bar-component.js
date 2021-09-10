4a9d23d4b0ba000617fe2a4fc92063b9599bf339
bwaterman8 <bwaterman8@gmail.com>
2021-09-10 10:03:35 -0400

    take 2

--- font-end/main-bar-component.js
+++ font-end/main-bar-component.js
@@ -1,8 +1,13 @@
+
+
 const renderMain = () => {
     const sectionElement = document.createElement('section');
     sectionElement.classList.add('main-bar');
     const headElement = document.createElement('div');
-    headElement.classList.add('main-bar-head')
+    headElement.classList.add('main-bar-head');
+    const headElementText = document.createElement('h3');
+    headElementText.innerText = 'Spring-Artists';
+    headElement.appendChild(headElementText);
     sectionElement.appendChild(headElement);
 
     return sectionElement;
